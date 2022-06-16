0eb8d8ff80b18a538951c62eafeb234dd9f20732
chkhurramshahzad <khurramdev32@gmail.com>
2022-06-16 07:57:31 -0700

    changes

---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\FlutterSDK\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "E:\\Intership Data\\Project Task\\leave_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\FlutterSDK\\flutter"
+  "PROJECT_DIR=E:\\Intership Data\\Project Task\\leave_app"
+  "FLUTTER_ROOT=C:\\src\\FlutterSDK\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=E:\\Intership Data\\Project Task\\leave_app\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=E:\\Intership Data\\Project Task\\leave_app"
+  "FLUTTER_TARGET=E:\\Intership Data\\Project Task\\leave_app\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=E:\\Intership Data\\Project Task\\leave_app\\.dart_tool\\package_config.json"
+)
